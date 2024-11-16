798a9fa7e52cd770665435181e9f04ede9d975e8
deepanshu <dipanshuraz2@gmail.com>
2024-11-17 02:04:46 +0700

    ref: frontend

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,14 +3,13 @@ import { Box, Flex } from "@chakra-ui/react";
 
 import { Footer, Header, MainPane } from "@/components";
 import { FloatingNav } from "@/components/Home/FloatingNav";
-// import { HeroScrollDemo } from "@/components/Home/ContainerScroll";
-// import { WobbleCardDemo } from "@/components/Home/WobbleCard";
-// import { Partners } from "@/components/Home/Partners";
+import { HeroScrollDemo } from "@/components/Home/ContainerScroll";
+import { WobbleCardDemo } from "@/components/Home/WobbleCard";
+import { Partners } from "@/components/Home/Partners";
 
 export default function Home() {
   return (
     <Flex flexDirection="column" minHeight="100vh" className="pb-16">
-      
       <div className="hidden">
         <Header />
         <Box as="main" flex={1} p={4}>
@@ -22,9 +21,9 @@ export default function Home() {
 
       <FloatingNav />
 
-      {/* <HeroScrollDemo />
+      <HeroScrollDemo />
       <Partners />
-      <WobbleCardDemo /> */}
+      <WobbleCardDemo />
     </Flex>
   );
 }
